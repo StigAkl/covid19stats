9f7e4ea309438c9cb3fa547052de747b12aea97a
StigAkl <stg@hotmail.no>
2020-03-24 00:05:17 +0100

    datafetch

---
+++ routes/home.js
@@ -0,0 +1,29 @@
+const express = require("express"); 
+const router = express.Router(); 
+const sqlite = require("sqlite3").verbose(); 
+const dbString = process.env.DATABASE_STRING || "dev_db"; 
+
+const db = new sqlite.Database(dbString); 
+
+router.get("/", async (req, res) => {
+
+    await db.run('CREATE TABLE IF NOT EXISTS countries (name TEXT PRIMARY KEY, confirmed_cases INTEGER, total_population INTEGER, confirmed_deaths INTEGER)');
+
+    let sql = `SELECT * FROM countries WHERE name='Norway'`;
+
+    db.get(sql, [], (err, row) => {
+        if(err) {
+            res.status(500).send("Internal server error"); 
+            throw err; 
+        }
+        const confirmed_cases = row.confirmed_cases; 
+        const confirmed_deaths = row.confirmed_deaths; 
+
+        res.render("index", {confirmed_cases: confirmed_cases, confirmed_deaths: confirmed_deaths}); 
+        
+    })
+});
+
+
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,26 +1,46 @@
 const express = require("express"); 
 const app = express(); 
-const path = require("path")
+const request = require("request"); 
+const homeRoutes = require("./routes/home");
+const dataUrl = "https://redutv-api.vg.no/corona/v1/sheets/norway-region-data";
+const sqlite3 = require("sqlite3").verbose(); 
+const dbString = process.env.DATABASE_STRING || "dev_db"; 
 
+const db = new sqlite3.Database(dbString); 
+
+const port = process.env.PORT || 80;
 
 app.set("view engine", "ejs"); 
 
-app.get("/", (req, res, next) => {
-    const labels1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const labels2 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const labels3 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-    const labels4 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const labels5 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-    const labels6 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const labels7 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-    const labels8 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-    const labels9 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-    res.render("test", {labels: [labels1, labels2, labels3, labels4, labels5, labels6, labels7, labels8, labels9,
-        ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"],
-        ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"]
-    ]})
-})
+app.use(homeRoutes); 
 
-app.listen(80, () => {
+// app.get("/", (req, res, next) => {
+//     const labels1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+//     const labels2 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+//     const labels3 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+//     const labels4 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+//     const labels5 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+//     const labels6 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+//     const labels7 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+//     const labels8 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+//     const labels9 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+//     res.render("test", {labels: [labels1, labels2, labels3, labels4, labels5, labels6, labels7, labels8, labels9,
+//         ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"],
+//         ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"]
+//     ]})
+// })
+
+async function fetchData() {
+    setInterval(() => {
+        request.get(dataUrl, (error, response, body) => {
+            let json = JSON.parse(body); 
+            const update = "UPDATE countries SET confirmed_cases=?,confirmed_deaths=? WHERE name='Norway'"
+            db.run(update, [json.metadata.confirmed.total, json.metadata.dead.total]); 
+        })
+    },1200000);
+}
+
+app.listen(port, async () => {
+    await fetchData();
     console.log("Listening on port 3000"); 
 })
