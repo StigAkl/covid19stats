9ca0df5ce67a8a50b28ddb91cbae159254e1203d
StigAkl <stg@hotmail.no>
2020-03-24 02:02:19 +0100

    charts

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ app.set("view engine", "ejs");
 
 app.use(homeRoutes); 
 
-// app.get("/", (req, res, next) => {
-//     const labels1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-//     const labels2 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-//     const labels3 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-//     const labels4 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-//     const labels5 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-//     const labels6 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-//     const labels7 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-//     const labels8 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-//     const labels9 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
-//     res.render("test", {labels: [labels1, labels2, labels3, labels4, labels5, labels6, labels7, labels8, labels9,
-//         ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"],
-//         ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"]
-//     ]})
-// })
+app.get("/charts", (req, res, next) => {
+    const labels1 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const labels2 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const labels3 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+    const labels4 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const labels5 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+    const labels6 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const labels7 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+    const labels8 = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const labels9 = ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"];
+    res.render("test", {labels: [labels1, labels2, labels3, labels4, labels5, labels6, labels7, labels8, labels9,
+        ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"],
+        ['Monday', "Tuesday", "Wednesday", "Thursday", "Friday", "Saturydag", "Sunday"]
+    ]})
+})
 
 async function fetchData() {
         request.get(dataUrl, (error, response, body) => {
